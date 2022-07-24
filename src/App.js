b83e614d4facd2e899d7ab51adf712ba10780d4b
unknown <arturohernandezvidal.ahv@gmail.com>
2022-07-24 16:14:42 -0500

    app elements in order

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import './App.css';
+import TimerSetting from './components/TimerSetting';
 
 class App extends React.Component{
   constructor(props){
     super(props);
-    
+    this.state={
+      breakCount: 5,
+      sessionCount: 25,
+      currentTimer: "Session",
+      
+    }
   }
   render(){
     return (
     <div className="app-container">
       <div id="setting-container">
-        <TimerSetting />
-        <TimerSetting />
+        <TimerSetting title="Work minutes" preset={this.state.sessionCount} />
+        <TimerSetting title="Break minutes" preset={this.state.breakCount} />
       </div>
       <div id="timer-container">
-        
+        <h3 id="timer-label">{this.state.currentTimer}</h3>
+        <h1 id="time-left">00:00</h1>
+        <div className="buttons">
+          <button id="start-stop"></button>
+          <button id="reset"></button>
+        </div>
       </div>
     </div>
   );
--- src/components/TimerSetting.jsx
+++ src/components/TimerSetting.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import '../styles/TimerSetting.css';
 
 function TimerSetting(props){
   return(
     <div>
-      
+      <h3 id={props.title==="Work minutes" ? "session-label" : "break-label"}>{props.title}</h3>
+      <div id="set-minutes">
+        <h4 id={props.title==="Work minutes" ? "session-increment" : "break-increment"}>+</h4>
+        <h4 id={props.title==="Work minutes" ? "session-length" : "break-length"}>{props.preset}</h4>
+        <h4 id={props.title==="Work minutes" ? "session-decrement" : "break-decrement"}>-</h4>
+      </div>
     </div>
   );
 }
+
+export default TimerSetting;
