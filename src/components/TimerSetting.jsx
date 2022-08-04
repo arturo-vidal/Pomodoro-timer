5f48dc4e672a7d5d1983f39f7f784692fe2d48b4
unknown <arturohernandezvidal.ahv@gmail.com>
2022-08-03 23:46:20 -0500

    styles

--- src/components/TimerSetting.jsx
+++ src/components/TimerSetting.jsx
@@ -6,9 +6,9 @@ function TimerSetting(props){
     <div>
       <h3 id={props.title==="Work minutes" ? "session-label" : "break-label"}>{props.title}</h3>
       <div id="set-minutes">
-        <h4 id={props.title==="Work minutes" ? "session-increment" : "break-increment"} onClick={props.addMin}>+</h4>
+        <h4 id={props.title==="Work minutes" ? "session-increment" : "break-increment"} className="moreless" onClick={props.addMin}>+</h4>
         <h4 id={props.title==="Work minutes" ? "session-length" : "break-length"}>{props.preset}</h4>
-        <h4 id={props.title==="Work minutes" ? "session-decrement" : "break-decrement"} onClick={props.sustrMin}>-</h4>
+        <h4 id={props.title==="Work minutes" ? "session-decrement" : "break-decrement"} className="moreless" onClick={props.sustrMin}>-</h4>
       </div>
     </div>
   );
